a38f64dae7721e287cbc1e566bc67634b5fcbf13
Sizwe Matenjwa <SizweMatenjwa100@gmail.com>
2024-11-27 23:19:15 +0200

    Firebase Authitication Services

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,17 @@
+class AuthService{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> _signInWithPassword(String email,String password)async{
+    try{
+      UserCredential result = await _auth.signInwithEmailAndPassword(email:email,password:password),
+      User? user = result.user;
+      return user;
+
+    }
+    catch(e){
+      print(e.ToString());
+      return null;
+    }
+  }
+git
+}
